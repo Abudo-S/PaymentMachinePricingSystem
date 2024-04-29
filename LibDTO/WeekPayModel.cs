97409c5c009acdf0c96d4068157d9cfa6cde703c
Abudo-S <abudo.mohamed123456789@gmail.com>
2024-04-29 12:08:48 +0200

    Complete WeekPayModel.proto. Adjust LibDTO.WeekPayModel. Implement WeekPayModelDbService.

--- LibDTO/WeekPayModel.cs
+++ LibDTO/WeekPayModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,26 +11,45 @@ namespace LibDTO
     /// <summary>
     /// validation rule: assigned dayRate (ex. MondayRateId) should matches a DayRate's assigned-correspondent weekDay
     /// </summary>
+    [BsonIgnoreExtraElements]
     public class WeekPayModel
     {
-        public int Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement]
         public int? MondayRateId { get; set; }
+
+        [BsonElement]
         public int? TuesdayRateId { get; set; }
+
+        [BsonElement]
         public int? WednesdayRateId { get; set; }
+
+        [BsonElement]
         public int? ThursdayRateId { get; set; }
+
+        [BsonElement]
         public int? FridayRateId { get; set; }
+
+        [BsonElement]
         public int? SaturdayRateId { get; set; }
+
+        [BsonElement]
         public int? SundayRateId { get; set; }
 
         /// <summary>
         /// to be applied on the sum of all day fees
         /// ex. 30.5% * (2 days' fee)
         /// </summary>
+        [BsonElement]
         public double WeekDayPercentBonus { get; set; }
 
         /// <summary>
         /// In case of non-declared week-day rate
         /// </summary>
+        [BsonElement]
         public required int WeekDayStandardRateId { get; set; }
 
     }
---
+++ WeekPayModelService/DbServices/WeekPayModelDbService.cs
@@ -0,0 +1,66 @@
+﻿using LibDTO;
+using LibDTO.Config;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace WeekPayModelService.DbServices
+{
+    public class WeekPayModelDbService
+    {
+        private readonly IMongoCollection<WeekPayModel> weekPayModelCollection;
+
+        public WeekPayModelDbService(
+            IOptions<PricingSystemDataBaseConfig> pricingSystemDataBaseConfig)
+        {
+            var mongoClient = new MongoClient(
+                pricingSystemDataBaseConfig.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                pricingSystemDataBaseConfig.Value.DatabaseName);
+
+            //mongoDatabase.CreateCollection(pricingSystemDataBaseConfig.Value.WeekPayModelCollectionName);
+
+            weekPayModelCollection = mongoDatabase.GetCollection<WeekPayModel>(
+                pricingSystemDataBaseConfig.Value.WeekPayModelCollectionName);
+        }
+
+        public async Task<List<WeekPayModel>> GetAllAsync() =>
+            await weekPayModelCollection.Find(_ => true).ToListAsync();
+
+        public async Task<WeekPayModel?> GetAsync(string id) =>
+            await weekPayModelCollection.Find(x => x.Id == StringTo24Hex(id)).FirstOrDefaultAsync();
+
+        public async Task<bool> CreateAsync(WeekPayModel weekPayModel) 
+        {
+            try
+            {
+                weekPayModel.Id = StringTo24Hex(weekPayModel.Id);
+                await weekPayModelCollection.InsertOneAsync(weekPayModel);
+                return true;
+            }
+            catch (Exception _) { }
+            return false;
+        }
+
+
+        public async Task<bool> UpdateAsync(string id, WeekPayModel weekPayModel)
+        {
+            try
+            {
+                weekPayModel.Id = StringTo24Hex(weekPayModel.Id);
+                return (await weekPayModelCollection.ReplaceOneAsync(x => x.Id == StringTo24Hex(id), weekPayModel)).IsAcknowledged;
+            }
+            catch (Exception _) { }
+            return false;
+        }
+
+        public async Task<bool> RemoveAsync(string id) =>
+            (await weekPayModelCollection.DeleteOneAsync(x => x.Id == StringTo24Hex(id))).IsAcknowledged;
+
+
+        public static string StringTo24Hex(string id)
+        {
+            return Int32.TryParse(id, out _)? Int32.Parse(id).ToString("x").PadLeft(24, '0').ToUpper() : id;
+        }
+    }
+}
