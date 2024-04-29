ff2e12e3c31a5743e8cfa0c481c19c1a7039bba8
Abudo-S <abudo.mohamed123456789@gmail.com>
2024-04-29 17:06:05 +0200

    Complete TimeInterval.proto. Adjust LibDTO.TimeInterval. Implement TimeIntervalDbService.

--- LibDTO/TimeInterval.cs
+++ LibDTO/TimeInterval.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +8,27 @@ using System.Threading.Tasks;
 
 namespace LibDTO
 {
+    [BsonIgnoreExtraElements]
     public class TimeInterval
     {
-        public int Id { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement]
         public TimeSpan From {  get; set; }
+
+        [BsonElement]
         public TimeSpan To { get; set; }
+
+        [BsonElement]
         public List<TimeIntervalRule> Rules { get; set; } = new();
 
         /// <summary>
         /// if it contains a value then it's related to a specific day; otherwise, it's applied to all days generally
         /// </summary>
+        [BsonElement]
         public int? DayPriceRateId { get; set; }
     }
 }
--- LibDTO/TimeIntervalRule.cs
+++ LibDTO/TimeIntervalRule.cs
@@ -1,25 +1,32 @@
-﻿namespace LibDTO
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace LibDTO
 {
+    [BsonIgnoreExtraElements]
     public class TimeIntervalRule
     {
         /// <summary>
         /// rule's priority in case of multiple rules covering a certain duration
         /// </summary>
+        [BsonElement]
         public int Priority { get; set; }
 
         /// <summary>
         /// price
         /// </summary>
+        [BsonElement]
         public double Amount { get; set; }
 
         /// <summary>
         /// minutes covered by amount
         /// </summary>
+        [BsonElement]
         public int FrequencyIntervalPerAmount { get; set; }
 
         /// <summary>
         /// total minutes coverable by this rule
         /// </summary>
+        [BsonElement]
         public int TotalCoveredMinutes { get; set; }
     }
 }
